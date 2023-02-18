fa91f5a9fb59153e98ffa445cd81e7b340ef587e
LuisCMry7 <jorgecasas.isc@gmail.com>
2023-02-18 13:12:54 -0600

    Create folder with name of file
    Create folder with CSV file name and create qr images inside new folder

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
-const QRCode = require('qrcode')
-const fs = require('fs');
+const QRCode = require("qrcode");
+const fs = require("fs");
 const csv = require("csv-parser");
 const args = process.argv;
-console.log(args);
-
 
 const opts = {
-    errorCorrectionLevel: 'H',
-    type: 'image/png',
-    quality: 0.95,
-    margin: 1,
-    color: {
-     dark: '#ffffff',
-     light: '#000000',
-    },
-   }
+  errorCorrectionLevel: "H",
+  type: "image/png",
+  quality: 0.95,
+  margin: 1,
+  color: {
+    dark: "#FFFFFF",
+    light: "#000000",
+  },
+};
 
+var files = fs.readdirSync('./');
+console.log(files);
 
-// ask for a file
-// hacer que el folder name sea el FileName
-// 
-
-
-
+if(files.filter(file => file.includes('.csv'))){
+  console.log('found');
+  const foundFile = files.filter(file => file.includes('.csv'));
+  console.log(foundFile);
+  const fileName = foundFile.toString();
 
 const results = [];
-fs.createReadStream("data.csv")
+fs.createReadStream(fileName)
   .pipe(csv())
   .on("data", (data) => results.push(data))
   .on("end", () => {
-
     results.forEach((element) => {
-        const qrCodeText = element.QR;
-        const ID = element.ID2;
-        const filepath = "./GeneratedCodes/" + ID + ".png"
-        // console.log(ID);
-        const file = fs.createWriteStream(filepath);
-        const code =  QRCode.toFileStream(file, qrCodeText, opts);
-        code;
-        
+      const qrCodeText = element.QR;
+      const ID = element.ID2;
+      const filepath = "./GeneratedCodes/" + fileName + "/" + ID + ".png";
+      const file = fs.createWriteStream(filepath);
+      const code = QRCode.toFileStream(file, qrCodeText, opts);
+      code;
     });
+    const folderName = fileName;
+    const folderPath = "./GeneratedCodes/" + folderName + "/";
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+    } else {
+      console.log("Folder already exists");
+    }
   });
+
+} else {
+  console.log('not found');
+}
